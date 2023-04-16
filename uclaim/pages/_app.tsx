a942357b777cbb942be880e7b3b80775b3521193
Trevor <tdohm@smu.edu>
2023-04-16 04:24:04 -0500

    Frontend!

--- uclaim/pages/_app.tsx
+++ uclaim/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from "next-auth/react";
+import React, { useState, useEffect } from "react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
